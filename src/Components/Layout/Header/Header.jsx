910b4131fd8be50eae8d752e65b87090db611757
vndrewhk <drewhkm@gmail.com>
2022-03-23 17:36:37 -0400

    pointer cursor on title

--- src/Components/Layout/Header/Header.jsx
+++ src/Components/Layout/Header/Header.jsx
@@ -1,24 +1,24 @@
-import { Button, Typography } from "@mui/material";
-import { style } from "@mui/system";
-import { useState } from "react";
-import HeaderButton from "../../UI/HeaderButton";
+// import { Button, Typography } from "@mui/material";
+// import { style } from "@mui/system";
+// import { useState } from "react";
+// import HeaderButton from "../../UI/HeaderButton";
 import styles from "./Header.module.css";
 import "@fontsource/pangolin";
-import SettingsIcon from "@mui/icons-material/Settings";
+// import SettingsIcon from "@mui/icons-material/Settings";
 import { useSelector } from "react-redux";
 
 const Header = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  // const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const loginHandler = () => {
-    setIsLoggedIn(true);
-    console.log("Logging in...");
-  };
+  // const loginHandler = () => {
+  //   setIsLoggedIn(true);
+  //   console.log("Logging in...");
+  // };
 
-  const logoutHandler = () => {
-    setIsLoggedIn(false);
-    console.log("Logged out!");
-  };
+  // const logoutHandler = () => {
+  //   setIsLoggedIn(false);
+  //   console.log("Logged out!");
+  // };
 
   const timerMode = useSelector((state) => state.mode);
   const reduxTimerValue = `time-${timerMode.mode.toString()}`;
@@ -28,7 +28,9 @@ const Header = () => {
   return (
     <div className={appClasses}>
       <div className={styles.headerContainer}>
-        <h1 className={styles.title}>Pomodoreact</h1>
+        <h1 className={styles.title} onClick={console.log("lol")}>
+          Pomodoreact
+        </h1>
         {/* <Typography variant="h1">Pomodoreact</Typography> */}
         {/* <div className={styles.headerLinks}>
           <HeaderButton variant="contained" disableElevation>
